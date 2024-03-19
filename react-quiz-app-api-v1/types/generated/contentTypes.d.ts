9e15db3a2d554112479094e61a33d15c72f15399
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-03-19 13:14:38 +0100

    updated

--- react-quiz-app-api-v1/types/generated/contentTypes.d.ts
+++ react-quiz-app-api-v1/types/generated/contentTypes.d.ts
@@ -362,6 +362,76 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiHtmlQuestionHtmlQuestion extends Schema.CollectionType {
+  collectionName: 'html_questions';
+  info: {
+    singularName: 'html-question';
+    pluralName: 'html-questions';
+    displayName: 'htmlQuestions';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    question: Attribute.String;
+    level: Attribute.String;
+    options: Attribute.JSON;
+    answer: Attribute.Text;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::html-question.html-question',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::html-question.html-question',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiUserAccountUserAccount extends Schema.CollectionType {
+  collectionName: 'user_accounts';
+  info: {
+    singularName: 'user-account';
+    pluralName: 'user-accounts';
+    displayName: 'UserAccounts';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    fullName: Attribute.String;
+    userName: Attribute.String;
+    email: Attribute.Email;
+    mobileNumber: Attribute.BigInteger;
+    password: Attribute.Password;
+    repeatPassword: Attribute.Password;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::user-account.user-account',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::user-account.user-account',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -678,7 +748,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     username: Attribute.String &
@@ -707,6 +776,8 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.role'
     >;
+    fullname: Attribute.String;
+    mobilenumber: Attribute.BigInteger;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
@@ -768,74 +839,6 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
-export interface ApiHtmlQuestionHtmlQuestion extends Schema.CollectionType {
-  collectionName: 'html_questions';
-  info: {
-    singularName: 'html-question';
-    pluralName: 'html-questions';
-    displayName: 'htmlQuestions';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    question: Attribute.String;
-    level: Attribute.String;
-    options: Attribute.JSON;
-    answer: Attribute.Text;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::html-question.html-question',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::html-question.html-question',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiUserAccountUserAccount extends Schema.CollectionType {
-  collectionName: 'user_accounts';
-  info: {
-    singularName: 'user-account';
-    pluralName: 'user-accounts';
-    displayName: 'UserAccounts';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    fullName: Attribute.String;
-    userName: Attribute.String;
-    email: Attribute.Email;
-    mobileNumber: Attribute.BigInteger;
-    password: Attribute.Password;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::user-account.user-account',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::user-account.user-account',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -846,6 +849,8 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::html-question.html-question': ApiHtmlQuestionHtmlQuestion;
+      'api::user-account.user-account': ApiUserAccountUserAccount;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
@@ -854,8 +859,6 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
-      'api::html-question.html-question': ApiHtmlQuestionHtmlQuestion;
-      'api::user-account.user-account': ApiUserAccountUserAccount;
     }
   }
 }
